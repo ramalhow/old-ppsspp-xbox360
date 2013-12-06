Ced2911/ppsspp

[core] some endian fix try

Core/HLE/sceIo.cpp
@@ -2057,7 +2057,7 @@ u32 sceIoIoctlAsync(u32 id, u32 cmd, u32 indataPtr, u32 inlen, u32 outdataPtr, u
 u32 sceIoGetFdList(u32 outAddr, int outSize, u32 fdNumAddr) {
 	WARN_LOG(SCEIO, "sceIoGetFdList(%08x, %i, %08x)", outAddr, outSize, fdNumAddr);
 
-	PSPPointer<SceUID> out;
+	PSPPointer<s32_le> out;
 	out = outAddr;
 	int count = 0;
 

ramalhow/old-ppsspp-xbox360

Core/HLE/sceIo.cpp
@@ -2060,7 +2060,7 @@ u32 sceIoIoctlAsync(u32 id, u32 cmd, u32 indataPtr, u32 inlen, u32 outdataPtr, u
 u32 sceIoGetFdList(u32 outAddr, int outSize, u32 fdNumAddr) {
 	WARN_LOG(SCEIO, "sceIoGetFdList(%08x, %i, %08x)", outAddr, outSize, fdNumAddr);
 
-	PSPPointer<SceUID> out;
+	PSPPointer<SceUID_le> out;
 	out = outAddr;
 	int count = 0;
 